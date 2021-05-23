2da2221b9ed412e4f4d7999c85dd34786ede6a0a
Loïs Postula <lois@postu.la>
2021-05-23 18:55:22 +0200

    [enh] Add ga and cookie law

--- frontend/App.vue
+++ frontend/App.vue
@@ -7,14 +7,31 @@
             </fade-transition>
         </main>
         <router-view name="footer"></router-view>
+        <cookie-law
+            @accept="acceptCookie"
+            buttonText="J'ai compris!"
+            message="Ce site Web utilise des cookies pour vous garantir la meilleure expérience possible sur notre site."
+        >
+        </cookie-law>
     </div>
 </template>
 <script>
 import { FadeTransition } from "vue2-transitions";
+import CookieLaw from 'vue-cookie-law';
+import { bootstrap } from 'vue-gtag'
 
 export default {
   components: {
-    FadeTransition
+    FadeTransition,
+    CookieLaw,
+  },
+  methods: {
+    acceptCookie() {
+      bootstrap()
+    },
+    refuseCookie() {
+
+    }
   }
 };
 </script>
--- frontend/main.js
+++ frontend/main.js
@@ -5,6 +5,8 @@ import router from "./router";
 import store from './store'
 import Axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueGtag from "vue-gtag";
+
 import Argon from "./plugins/argon-kit";
 import * as backendAPI from './common/backendAPI'
 import titleMixin from './mixins/titleMixin';
@@ -16,6 +18,10 @@ Vue.prototype.$api = backendAPI;
 Vue.use(Argon);
 Vue.use(VueAxios, Axios)
 Vue.mixin(titleMixin);
+Vue.use(VueGtag, {
+  config: {id: 'G-LF0NFE38PY'},
+  bootstrap: false,
+}, router);
 Sentry.init({
   Vue: Vue,
   dsn: "https://d5d4f8ab79e44e238be6d6e0253e8144@o207892.ingest.sentry.io/5583101",
